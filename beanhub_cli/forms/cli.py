a3dfe5b171876ef87016e8c284b9913c980f0a5d
Fang-Pen Lin <hello@fangpenlin.com>
2023-10-05 16:08:36 -0700

    Add help info

--- beanhub_cli/forms/cli.py
+++ beanhub_cli/forms/cli.py
@@ -5,7 +5,9 @@ from ..context import Context
 from ..context import pass_context
 
 
-@root_cli.group(name="forms")
+@root_cli.group(
+    name="forms", help="Validating BeanHub Forms and running a simple web app."
+)
 @pass_context
 def cli(ctx: Context):
     pass
