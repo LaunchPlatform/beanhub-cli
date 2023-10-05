4d25fdf5596408e6a9f9408e3424e3a3f05562da
Fang-Pen Lin <hello@fangpenlin.com>
2023-10-05 16:13:06 -0700

    Show default

--- beanhub_cli/forms/main.py
+++ beanhub_cli/forms/main.py
@@ -7,13 +7,24 @@ from .cli import cli
 
 @cli.command()
 @click.option(
-    "-p", "--port", default=8080, help="Port number for serving the forms web app."
+    "-p",
+    "--port",
+    default=8080,
+    show_default=True,
+    help="Port number for serving the forms web app.",
 )
 @click.option(
-    "-h", "--host", default="127.0.0.1", help="Host for serving the forms web app."
+    "-h",
+    "--host",
+    default="127.0.0.1",
+    show_default=True,
+    help="Host for serving the forms web app.",
 )
 @click.option(
-    "--uvicorn-log-level", default="info", help="Log level for Uvicorn web server."
+    "--uvicorn-log-level",
+    default="info",
+    show_default=True,
+    help="Log level for Uvicorn web server.",
 )
 def server(port: int, host: str, uvicorn_log_level: str):
     app = make_app()
