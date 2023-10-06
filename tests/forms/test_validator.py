0eecbcb7d35c6463b39dba6b640e751b978206db
Fang-Pen Lin <hello@fangpenlin.com>
2023-10-05 17:08:35 -0700

    Add test for validation

--- tests/forms/test_validator.py
+++ tests/forms/test_validator.py
@@ -1,12 +1,66 @@
 import pathlib
 
 import pytest
+import yaml
+from pydantic import ValidationError
 
 from beanhub_cli.forms.validator import validate_doc
 
 
 def test_file_does_not_exist(tmp_path: pathlib.Path):
     doc_file = tmp_path / "does-not-exists.yaml"
-    with pytest.raises(ValueError) as error:
+    with pytest.raises(ValueError) as exc:
         validate_doc(doc_file)
-    assert error.value.args[0] == f"Form document at {doc_file} does not exist"
+    assert exc.value.args[0] == f"Form document at {doc_file} does not exist"
+
+
+@pytest.mark.parametrize(
+    "schema, expected_errors",
+    [
+        (
+            {},
+            [
+                {
+                    "type": "missing",
+                    "loc": ("forms",),
+                    "msg": "Field required",
+                    "input": {},
+                    "url": "https://errors.pydantic.dev/2.4/v/missing",
+                }
+            ],
+        ),
+        (
+            dict(forms=[dict()]),
+            [
+                {
+                    "type": "missing",
+                    "loc": ("forms", 0, "name"),
+                    "msg": "Field required",
+                    "input": {},
+                    "url": "https://errors.pydantic.dev/2.4/v/missing",
+                },
+                {
+                    "type": "missing",
+                    "loc": ("forms", 0, "fields"),
+                    "msg": "Field required",
+                    "input": {},
+                    "url": "https://errors.pydantic.dev/2.4/v/missing",
+                },
+                {
+                    "type": "missing",
+                    "loc": ("forms", 0, "operations"),
+                    "msg": "Field required",
+                    "input": {},
+                    "url": "https://errors.pydantic.dev/2.4/v/missing",
+                },
+            ],
+        ),
+    ],
+)
+def test_bad_schema(tmp_path: pathlib.Path, schema: dict, expected_errors: list):
+    doc_file = tmp_path / "forms.yaml"
+    with doc_file.open("wt") as fo:
+        yaml.dump(schema, fo)
+    with pytest.raises(ValidationError) as exc:
+        validate_doc(doc_file)
+    assert exc.value.errors() == expected_errors
