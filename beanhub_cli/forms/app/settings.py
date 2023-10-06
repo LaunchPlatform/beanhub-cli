eb92b80bc70a5dc5c8c5f80b66f5cfb0c6fcb65a
Fang-Pen Lin <hello@fangpenlin.com>
2023-10-05 23:33:41 -0700

    Fix pydantic

--- beanhub_cli/forms/app/settings.py
+++ beanhub_cli/forms/app/settings.py
@@ -1,18 +1,17 @@
 import pathlib
 import secrets
 
+from pydantic import ConfigDict
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    model_config = ConfigDict(case_sensitive=True)
     SITE_NAME: str = "BeanHub Forms"
     DEBUG_LOG: bool = False
     SECRET_KEY: str = secrets.token_urlsafe(32)
     SESSION_MAX_AGE: int = 14 * 24 * 60 * 60  # 14 days, in seconds
     BEANCOUNT_DIR: pathlib.Path = pathlib.Path.cwd()
-
-    class Config:
-        case_sensitive = True
 
 
 settings = Settings()
