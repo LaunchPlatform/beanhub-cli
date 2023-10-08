56bef543d84f03a620c4e701cb6f4225abcb6127
Fang-Pen Lin <hello@fangpenlin.com>
2023-10-08 11:25:36 -0700

    Remove unused stuff

--- beanhub_cli/forms/cli.py
+++ beanhub_cli/forms/cli.py
@@ -1,19 +1,11 @@
-from click import pass_context as pass_click_context
-from click.core import Context as ClickContext
-
 from ..aliase import AliasedGroup
 from ..cli import cli as root_cli
-from ..environment import Environment
-from ..environment import pass_env
 
 
 @root_cli.group(
     name="form",
     help="Validating BeanHub Forms and running a simple web app.",
-    context_settings=dict(obj="bar"),
     cls=AliasedGroup,
 )
-@pass_env
-@pass_click_context
-def cli(ctx: Environment, click_context: ClickContext):
+def cli():
     pass
