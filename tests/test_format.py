0219db54f56081563816be1b90e7360d3be31221
Fang-Pen Lin <hello@fangpenlin.com>
2024-06-27 20:04:14 -0700

    Add test for default format

--- tests/test_format.py
+++ tests/test_format.py
@@ -1,5 +1,4 @@
 import pathlib
-import textwrap
 
 from click.testing import CliRunner
 
@@ -12,16 +11,27 @@ def test_format_cmd(tmp_path: pathlib.Path, cli_runner: CliRunner):
     beanhub_dir.mkdir()
 
     bean_file = beanhub_dir / "sample.bean"
-    bean_file.write_text(
-        textwrap.dedent(
-            """\
-    2024-06-27   open   Assets:Cash
-    """
-        )
-    )
+    bean_file.write_text("2024-06-27   open   Assets:Cash")
 
     cli_runner.mix_stderr = False
     with switch_cwd(tmp_path):
         result = cli_runner.invoke(cli, ["format", str(bean_file)])
     assert result.exit_code == 0
     assert bean_file.read_text() == "2024-06-27 open Assets:Cash\n"
+
+
+def test_format_cmd_without_args(tmp_path: pathlib.Path, cli_runner: CliRunner):
+    beanhub_dir = tmp_path / ".beanhub"
+    beanhub_dir.mkdir()
+
+    included_bean = beanhub_dir / "mybook.bean"
+    included_bean.write_text("2024-06-27   open   Assets:Cash")
+
+    main_bean = beanhub_dir / "main.bean"
+    main_bean.write_text('include "mybook.bean"')
+
+    cli_runner.mix_stderr = False
+    with switch_cwd(beanhub_dir):
+        result = cli_runner.invoke(cli, ["format"])
+    assert result.exit_code == 0
+    assert included_bean.read_text() == "2024-06-27 open Assets:Cash\n"
