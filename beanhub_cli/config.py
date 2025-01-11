0682549ba42969ce89a0c304b302840b019eb082
Fang-Pen Lin <hello@fangpenlin.com>
2025-01-11 02:11:39 -0800

    Fix file mode

--- beanhub_cli/config.py
+++ beanhub_cli/config.py
@@ -34,7 +34,7 @@ def save_config(config: Config, file_path: pathlib.Path | None = None):
     file_path.parent.mkdir(parents=True, exist_ok=True)
 
     file_path.write_text("")
-    file_path.chmod(0o400)
+    file_path.chmod(0o600)
     # TODO: for now we save the config as plaintext. We should consider to use OS keychain service in the future
     #       for better security
     with file_path.open("wb") as fo:
