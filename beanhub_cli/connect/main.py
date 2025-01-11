363e2c828e97892b2c465c9e46ade4b3bcf42ef1
Fang-Pen Lin <hello@fangpenlin.com>
2025-01-11 11:34:34 -0800

    Print result better

--- beanhub_cli/connect/main.py
+++ beanhub_cli/connect/main.py
@@ -5,11 +5,19 @@ import urllib.parse
 
 import click
 import requests
+import rich
+from rich import box
+from rich.markup import escape
+from rich.padding import Padding
+from rich.table import Table
 
 from ..config import load_config
 from ..environment import Environment
 from ..environment import pass_env
 from .cli import cli
+
+TABLE_HEADER_STYLE = "yellow"
+TABLE_COLUMN_STYLE = "cyan"
 
 
 @enum.unique
@@ -59,11 +67,17 @@ def sync(env: Environment):
         env.api_base_url, f"v1/repos/{username}/{repo}/connect/sync_batches"
     )
     resp = requests.post(url, headers={"access-token": token})
-    # TODO: provide friendly error messages here
-    # TODO: print too soon error
+    if resp.status_code == 422:
+        env.logger.error("Failed to sync with error: %s", resp.json())
+        sys.exit(-1)
     resp.raise_for_status()
+
     batch_id = resp.json()["id"]
-    env.logger.info("Created sync batch %s, waiting for updates ...", batch_id)
+    env.logger.info(
+        "Created sync batch [green]%s[/], waiting for updates ...",
+        batch_id,
+        extra={"markup": True, "highlighter": None},
+    )
 
     url = urllib.parse.urljoin(
         env.api_base_url, f"v1/repos/{username}/{repo}/connect/sync_batches/{batch_id}"
@@ -87,13 +101,47 @@ def sync(env: Environment):
         )
         progress = len(good_terms) + len(bad_terms)
         if progress >= total:
-            # TODO: print better report
-            env.logger.info("Good: %s", good_terms)
-            env.logger.info("Bad: %s", bad_terms)
+            table = Table(
+                title="Sync finished successfully",
+                box=box.SIMPLE,
+                header_style=TABLE_HEADER_STYLE,
+                expand=True,
+            )
+            table.add_column("Id", style=TABLE_COLUMN_STYLE)
+            table.add_column("Institution", style=TABLE_COLUMN_STYLE)
+            table.add_column("State", style=TABLE_COLUMN_STYLE)
+            for sync in good_terms:
+                table.add_row(
+                    escape(sync["id"]),
+                    escape(sync["item"]["institution_name"]),
+                    escape(sync["state"]),
+                )
+            rich.print(Padding(table, (1, 0, 0, 4)))
+
+            table = Table(
+                title="Sync finished with error",
+                box=box.SIMPLE,
+                header_style=TABLE_HEADER_STYLE,
+                expand=True,
+            )
+            table.add_column("Id", style=TABLE_COLUMN_STYLE)
+            table.add_column("Institution", style=TABLE_COLUMN_STYLE)
+            table.add_column("State", style=TABLE_COLUMN_STYLE)
+            table.add_column("Error", style=TABLE_COLUMN_STYLE)
+            for sync in bad_terms:
+                table.add_row(
+                    escape(sync["id"]),
+                    escape(sync["item"]["institution_name"]),
+                    escape(sync["state"]),
+                    escape(sync["error_message"]),
+                )
+            rich.print(Padding(table, (1, 0, 0, 4)))
 
             env.logger.info("done")
             break
-        env.logger.info("Still processing, %s out of %s", progress, total)
+        env.logger.info(
+            "Still processing, [green]%s[/] out of [green]%s[/]", progress, total
+        )
 
 
 @cli.command(help="")
