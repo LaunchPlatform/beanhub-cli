f23f9c07f91629f7b548f91ba019446be0f02d1e
Fang-Pen Lin <hello@fangpenlin.com>
2025-01-12 16:37:41 -0800

    Add test for check_imports

---
+++ tests/test_utils.py
@@ -0,0 +1,24 @@
+import logging
+
+import pytest
+from pytest_mock import MockFixture
+
+from beanhub_cli.utils import check_imports
+
+
+def test_check_imports(mocker: MockFixture):
+    logger = logging.getLogger(__name__)
+    spy = mocker.spy(logger, "error")
+    with pytest.raises(SystemExit) as exp:
+        check_imports(
+            logger=logger,
+            module_names=["never_ever_exists0", "never_ever_exists1"],
+            required_extras=["extras0", "extras1"],
+        )
+    assert exp.value.args[0] == -1
+    spy.assert_called_once_with(
+        'Cannot import module %s, please ensure that you install beanhub-cli with optional deps [%s].Like `pip install "beanhub-cli[%s]"`',
+        ["never_ever_exists0", "never_ever_exists1"],
+        "extras0,extras1",
+        "extras0,extras1",
+    )
