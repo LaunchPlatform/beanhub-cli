9080223ca7dbaa11ac3d2f8d55e4b54be3ef6ff8
Fang-Pen Lin <hello@fangpenlin.com>
2025-04-17 21:09:34 -0700

    Move method around

--- beanhub_cli/file_io.py
+++ beanhub_cli/file_io.py
@@ -1,5 +1,7 @@
 import io
 import logging
+import pathlib
+import sys
 import tarfile
 
 
@@ -10,3 +12,44 @@ def extract_tar(input_file: io.BytesIO, logger: logging.Logger):
         else:
             logger.warning("Performing unsafe tar file extracting")
             tar_file.extractall()
+
+
+def extract_inbox_tar(
+    input_file: io.BytesIO,
+    email_output_paths: dict[str, pathlib.Path],
+    workdir_path: pathlib.Path,
+    unsafe_tar_extract: bool,
+    logger: logging.Logger,
+):
+    with tarfile.open(fileobj=input_file, mode="r:gz") as tar_file:
+        for member in tar_file:
+            if not member.isreg():
+                continue
+            member_path = pathlib.PurePosixPath(member.name)
+            email_id = member_path.stem
+            output_path = email_output_paths.get(email_id)
+            if output_path is None:
+                logger.error("Cannot find output path for email %s", email_id)
+                sys.exit(-1)
+            output_path.parent.mkdir(exist_ok=True, parents=True)
+            logger.info(
+                "Writing email [green]%s[/] to [green]%s[/]",
+                email_id,
+                output_path,
+                extra={"markup": True, "highlighter": None},
+            )
+            full_output_path = workdir_path / output_path
+            has_data_filter = hasattr(tarfile, "data_filter")
+            if not has_data_filter and not unsafe_tar_extract:
+                logger.error(
+                    "You need to use Python >= 3.11 in order to safely unpack the downloaded tar file, or you need to pass "
+                    "in --unsafe-tar-extract argument to allow unsafe tar file extracting"
+                )
+                sys.exit(-1)
+            member.name = output_path.name
+            tar_file.extract(
+                member,
+                full_output_path.parent,
+                set_attrs=False,
+                filter="data" if has_data_filter else None,
+            )
--- beanhub_cli/inbox/main.py
+++ beanhub_cli/inbox/main.py
@@ -1,4 +1,3 @@
-import io
 import json
 import logging
 import pathlib
@@ -117,46 +116,6 @@ def compute_missing_emails(
             yield inbox_email, rel_output_file
         else:
             raise ValueError(f"Unexpected action type {type(action)}")
-
-
-def extract_tar(
-    input_file: io.BytesIO,
-    email_output_paths: dict[str, pathlib.Path],
-    workdir_path: pathlib.Path,
-    unsafe_tar_extract: bool,
-):
-    with tarfile.open(fileobj=input_file, mode="r:gz") as tar_file:
-        for member in tar_file:
-            if not member.isreg():
-                continue
-            member_path = pathlib.PurePosixPath(member.name)
-            email_id = member_path.stem
-            output_path = email_output_paths.get(email_id)
-            if output_path is None:
-                logger.error("Cannot find output path for email %s", email_id)
-                sys.exit(-1)
-            output_path.parent.mkdir(exist_ok=True, parents=True)
-            logger.info(
-                "Writing email [green]%s[/] to [green]%s[/]",
-                email_id,
-                output_path,
-                extra={"markup": True, "highlighter": None},
-            )
-            full_output_path = workdir_path / output_path
-            has_data_filter = hasattr(tarfile, "data_filter")
-            if not has_data_filter and not unsafe_tar_extract:
-                logger.error(
-                    "You need to use Python >= 3.11 in order to safely unpack the downloaded tar file, or you need to pass "
-                    "in --unsafe-tar-extract argument to allow unsafe tar file extracting"
-                )
-                sys.exit(-1)
-            member.name = output_path.name
-            tar_file.extract(
-                member,
-                full_output_path.parent,
-                set_attrs=False,
-                filter="data" if has_data_filter else None,
-            )
 
 
 @cli.command(
@@ -497,6 +456,7 @@ def dump(
 
         # delay import for testing purpose
         from ..encryption import decrypt_file
+        from ..file_io import extract_inbox_tar
 
         decrypt_file(
             input_file=encrypted_file, output_file=decrypted_file, key=key, iv=iv
@@ -505,11 +465,12 @@ def dump(
             inbox_email.id: output_path
             for inbox_email, output_path in missing_email_output_files
         }
-        extract_tar(
+        extract_inbox_tar(
             input_file=decrypted_file,
             email_output_paths=email_output_paths,
             workdir_path=workdir_path,
             unsafe_tar_extract=unsafe_tar_extract,
+            logger=logger,
         )
 
     logger.info("done")
